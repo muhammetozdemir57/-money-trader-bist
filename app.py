e7693872c6f600891de4aca646cbfa1b44e2ec7e
muhammetozdemir57 <57muhammetozdemir57@gmail.com>
2026-07-04 12:02:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Money Trader BIST",
+    page_icon="💰",
+    layout="wide"
+)
+
+st.title("💰 Money Trader BIST")
+st.success("Uygulama başarıyla çalışıyor.")
+st.write("Bir sonraki adımda BIST tarama motorunu ekleyeceğiz.")
